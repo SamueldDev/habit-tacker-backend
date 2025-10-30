4f8dec5bd4891e9dc09bb73bbdb0a55a85e482ed
SamueldDev <youngsammy2018@gmail.com>
2025-10-30 19:08:45 +0100

    updated the db file

--- config/db.js
+++ config/db.js
@@ -4,10 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
+import { Sequelize } from "sequelize";
+
 const isProduction = process.env.NODE_ENV === "production";
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "mysql",
+  dialect: isProduction ? "postgres" : "mysql",
   logging: false,
   dialectOptions: isProduction
     ? {
